9817e802ff393d253a69257f8d5c62869900d56a
Roman Lesnoy <rmnlsn@yandex.ru>
2020-10-02 19:01:33 +0300

    final commit after second review

--- scripts/index.js
+++ scripts/index.js
@@ -5,19 +5,17 @@ let popup = document.querySelector('.popup');
 let currentProfileName = document.querySelector('.profile__name');
 let currentAboutMe = document.querySelector('.profile__about-me');
 
-let inputProfileName = document.querySelector('.popup_input-name');
-let inputAboutMe = document.querySelector('.popup_input-about-me');
+let inputProfileName = document.querySelector('.popup__input-name');
+let inputAboutMe = document.querySelector('.popup__input-about-me');
 
 let formElement = document.querySelector('.popup__form');
 
 function popupToggle () {
-    popup.classList.toggle('popup_opened'); 
-    if (popup.classList.contains('popup_opened') == true) {
-    inputProfileName.value = currentProfileName.textContent;
-    inputAboutMe.value = currentAboutMe.textContent;
-    } else {
-        
+    if (popup.classList.contains('popup_opened') === false) {
+        inputProfileName.value = currentProfileName.textContent;
+        inputAboutMe.value = currentAboutMe.textContent;
     }
+    popup.classList.toggle('popup_opened'); 
 }
 
 function formSubmitHandler (evt) {
