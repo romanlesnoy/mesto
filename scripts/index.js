5e61564c635e667d93f1dd5bd867b2ffc36ee4bf
Roman Lesnoy <rmnlsn@yandex.ru>
2020-10-09 13:02:53 +0300

    feat open and close btn

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,50 @@
-let openPopupButton = document.querySelector('.profile__edit-btn');
-let closePopupButton = document.querySelector('.popup__close-btn');
-let popup = document.querySelector('.popup');
+//Переменные popup редактирования профиля
+let popupEditProfile = document.querySelector('.popup_profile-edit');
+let openEditProfilePopupButton = document.querySelector('.profile__edit-btn');
+let closeEditProfilePopupButton = popupEditProfile.querySelector('.popup__close-btn');
+let formEditProfile = popupEditProfile.querySelector('.popup__form');
 
+//Переменные popup добавления карточки
 let popupAddCard = document.querySelector('.popup_add-card');
 let openAddCardPopupButton = document.querySelector('.profile__add-btn');
 let closeAddCardPopupButton = popupAddCard.querySelector('.popup__close-btn');
 
+//Значения профиля
 let currentProfileName = document.querySelector('.profile__name');
 let currentAboutMe = document.querySelector('.profile__about-me');
 
-let inputProfileName = document.querySelector('.popup__input-name');
-let inputAboutMe = document.querySelector('.popup__input-about-me');
+//Значения инпута popup редактирования профиля
+let inputProfileName = popupEditProfile.querySelector('.popup__input-name');
+let inputAboutMe = popupEditProfile.querySelector('.popup__input-about-me');
 
-let formElement = document.querySelector('.popup__form');
-
-function popupToggle () {
-    if (popup.classList.contains('popup_opened') === false) {
+//Функция открытия popup редактирования профиля 
+function popupEditProfileToggle () {
+    if (popupEditProfile.classList.contains('popup_opened') === false) {
         inputProfileName.value = currentProfileName.textContent;
         inputAboutMe.value = currentAboutMe.textContent;
     }
-    popup.classList.toggle('popup_opened'); 
+    popupEditProfile.classList.toggle('popup_opened'); 
 }
 
-function popupAddCardToggle () {
-    popupAddCard.classList.toggle('popup_opened'); 
-}
-
+//Функция сохранения данных инпута в профиль
 function formSubmitHandler (evt) {
     evt.preventDefault();
     currentProfileName.textContent = inputProfileName.value;
     currentAboutMe.textContent = inputAboutMe.value;
-    popupToggle(); 
+    popupEditProfileToggle(); 
 }
 
+//Функция открытия popup добавления карточки
+function popupAddCardToggle () {
+    popupAddCard.classList.toggle('popup_opened'); 
+}
 
-openPopupButton.addEventListener ('click', popupToggle);
+//Обработчики событий popup редактирования профиля
+openEditProfilePopupButton.addEventListener ('click', popupEditProfileToggle);
+closeEditProfilePopupButton.addEventListener ('click', popupEditProfileToggle);
+formEditProfile.addEventListener('submit', formSubmitHandler); 
+
+//Обработчики событий popup добавления карточки
 openAddCardPopupButton.addEventListener('click', popupAddCardToggle);
-closePopupButton.addEventListener ('click', popupToggle);
-closeAddCardPopupButton.addEventListener('click', popupAddCardToggle);
-formElement.addEventListener('submit', formSubmitHandler); 
+closeAddCardPopupButton.addEventListener ('click', popupAddCardToggle);
+
