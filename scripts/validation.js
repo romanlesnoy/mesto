78afc222798414700f5c3e7bb95314a1ef9471d4
Roman Lesnoy <rmnlsn@yandex.ru>
2020-10-23 17:29:30 +0300

    start validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,8 +1,15 @@
-enableValidation({
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input-field',
-    submitButtonSelector: '.popup__save-btn',
-    inactiveButtonClass: 'popup__save-btn_disabled',
-    inputErrorClass: 'popup__input-error'
-    //errorClass: 'popup__error_visible'
-  }); 
+const newPlaceForm = document.querySelector('.popup__form');
+
+const nameInput = newPlaceForm.querySelector('.popup__input-field');
+console.log(nameInput);
+
+nameInput.addEventListener()'input', () => {
+    console.log('input');
+}
+
+newPlaceForm.addEventListener('submit', ()=>{
+    //evt.preventDefault();
+    console.log('submit');
+    
+})
+
