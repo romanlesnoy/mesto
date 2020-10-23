77216e77c497e9a4e3f7ea123297e33aaf4f59b1
Roman Lesnoy <rmnlsn@yandex.ru>
2020-10-23 17:01:34 +0300

    create style files and import files, add atribute for input form, create and import validation.js

---
+++ scripts/validation.js
@@ -0,0 +1,8 @@
+enableValidation({
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input-field',
+    submitButtonSelector: '.popup__save-btn',
+    inactiveButtonClass: 'popup__save-btn_disabled',
+    inputErrorClass: 'popup__input-error'
+    //errorClass: 'popup__error_visible'
+  }); 
