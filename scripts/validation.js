76bc559060295e26cfb9d54dcb9ff8a5b85d0b42
Roman Lesnoy <rmnlsn@yandex.ru>
2020-10-24 14:55:40 +0300

    feature validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,15 +1,65 @@
-const newPlaceForm = document.querySelector('.popup__form');
-
-const nameInput = newPlaceForm.querySelector('.popup__input-field');
-console.log(nameInput);
-
-nameInput.addEventListener()'input', () => {
-    console.log('input');
+function showError(formElement, input){
+    const errorElement = formElement.querySelector(`#${input.id}-error`);
+    errorElement.textContent = input.validationMessage;
+    input.classList.add('popup__input-field_state_invalid');
+    
 }
 
-newPlaceForm.addEventListener('submit', ()=>{
-    //evt.preventDefault();
-    console.log('submit');
-    
-})
+function hideError(formElement, input) {
+    const errorElement = formElement.querySelector(`#${input.id}-error`);
+    errorElement.textContent = " ";
+    input.classList.remove('popup__input-field_state_invalid');
+}
 
+function checkInputValidity(formElement, input) {
+    if (input.checkValidity()) {
+        hideError (formElement, input);
+    } else {
+        showError(formElement, input);
+    }
+};
+
+function toggleButtonState(formElement, buttonElement) {
+    if (formElement.checkValidity()) {
+        buttonElement.classList.remove('popup__save-btn_disable');
+        buttonElement.disabled = false;
+    } else {
+        buttonElement.classList.add('popup__save-btn_disable');
+        buttonElement.disabled = true;
+    }
+}
+
+function setEventListeners(formElement) {
+    let inputElements = Array.from(formElement.querySelectorAll(validationElementsObject.inputSelector));
+    let buttonElement = formElement.querySelector(validationElementsObject.submitButtonSelector);
+
+    inputElements.forEach((input) => {
+        input.addEventListener ('input', () => {
+            checkInputValidity(formElement, event.target);
+            toggleButtonState(formElement, buttonElement);
+        });
+    });
+
+    toggleButtonState(formElement, buttonElement);
+}
+
+function enableValidation(validationElementsObject) {
+    const formElements = Array.from(document.querySelectorAll(validationElementsObject.formSelector));
+    console.log(formElements);
+
+    formElements.forEach( form => {
+        form.addEventListener('submit', () => {
+            event.preventDefault();
+        });
+
+        setEventListeners (validationElementsObject);
+    })
+}
+
+enableValidation({
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input-field',
+    submitButtonSelector: '.popup__save-btn',
+    inactiveButtonClass: 'popup__save-btn_disable',
+    inputErrorClass: 'popup__input-field_state_invalid',
+});
